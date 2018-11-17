4ae91e40b796fbad687670a78b60ba01c7efeaec
Praveenkumar <tpkprav@gmail.com>
2018-11-17 13:59:30 +0800

    Added hands on excercises part 1

---
+++ 002-handsonexcercises/001-excercise1/excercise1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	x = 42
+	y = "James Bond"
+	z = true
+	fmt.Println("The value of x is :", x)
+	fmt.Println("The value of y is :", y)
+	fmt.Println("The value of z is :", z)
+	fmt.Println("The value of x is :", x, "\n The value of y is :", y, "\n The value of z is :", z)
+}
