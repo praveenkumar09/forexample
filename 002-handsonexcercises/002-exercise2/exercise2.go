64fecbeb7354877598845b00b91285982c9368f0
Praveenkumar <tpkprav@gmail.com>
2018-11-17 14:14:54 +0800

    Excercise 2 is added and exercise 1 is fixed

--- 002-handsonexcercises/002-exercise2/exercise2.go
+++ 002-handsonexcercises/002-exercise2/exercise2.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-var x int
-var y string
-var z bool
-
 func main() {
+	x := 42
+	y := "James Bond"
+	z := true
 	fmt.Println("The value of x is :", x)
 	fmt.Println("The value of y is :", y)
 	fmt.Println("The value of z is :", z)
