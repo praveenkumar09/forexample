40bf9acf73eb8787ec024ec8735719286525f5c2
Praveenkumar <tpkprav@gmail.com>
2018-11-17 14:43:10 +0800

    exercise 5 is added

---
+++ 002-handsonexcercises/005-exercise5/exercise5.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type praveen int
+
+var x praveen
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+
+}
