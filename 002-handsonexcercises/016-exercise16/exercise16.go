dca63f53ea8d82346c3f8e8c87ec4f232f257d4f
Praveenkumar <tpkprav@gmail.com>
2018-11-21 01:13:19 +0800

    exercise 17 is added

--- 002-handsonexcercises/016-exercise16/exercise16.go
+++ 002-handsonexcercises/016-exercise16/exercise16.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	for i := 10; i <= 100; i++ {
-			fmt.Println(i%4)
+		fmt.Println(i % 4)
 	}
 }
---
+++ 002-handsonexcercises/017-exercise17/exercise17.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 4
+	y := 5
+	if x < y {
+		fmt.Println("X is less than y")
+	}
+}
