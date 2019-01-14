ed9e9b171fce6a32cf36070837ced4155ebdd311
Praveenkumar <tpkprav@gmail.com>
2019-01-14 15:08:13 +0800

    range Over Channels edited and sampleCheckGo added

---
+++ 007-channelsBlock/003-directionalChannel/sampleCheck.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	go foo1()
+	bar1()
+	fmt.Println("Code about to exit")
+
+}
+
+func foo1(){
+	fmt.Println("Hai there")
+}
+
+func bar1(){
+	fmt.Println("Hai secnd there")
+}
