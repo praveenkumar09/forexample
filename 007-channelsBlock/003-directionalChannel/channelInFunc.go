5d79655d017517b19256cb507e3c3586ae82d87f
Praveenkumar <tpkprav@gmail.com>
2019-01-16 12:35:22 +0800

    Different types of channel print go class is added

--- 007-channelsBlock/003-directionalChannel/channelInFunc.go
+++ 007-channelsBlock/003-directionalChannel/channelInFunc.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
 func main() {
+	// bi - directional channel
 	c := make(chan int)
-	go func() {
-		fmt.Println("The number of routines first time is ->", runtime.NumGoroutine())
-		c <- 42
-		c <- 43
-		fmt.Println("The value 43 is also added")
-	}()
-	fmt.Println("The number of routines second time is ->", runtime.NumGoroutine())
-	fmt.Println("The value inside the channel is ->", <-c)
-	fmt.Println("The value inside the channel is ->", <-c)
-	fmt.Println("The number of routines third time is ->", runtime.NumGoroutine())
+	fmt.Printf("%T\t%v\n", c, "Type of bidirectional channel")
+	// channel to set values to the channel
+	in := make(chan<- int)
+	fmt.Printf("%T\t%v\n", in, "Type of set only values to a channel")
+	// channel to recieve values from the channel
+	out := make(<-chan int)
+	fmt.Printf("%T\t%v\n", out, "Type of recieve only values to a channel")
+
 }
